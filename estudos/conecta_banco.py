b483964072e7666f66b6731c9d5da3e9b3bd7923
Denys Silva <denirow@gmail.com>
2021-12-18 10:58:16 -0300

    v01

--- estudos/conecta_banco.py
+++ estudos/conecta_banco.py
@@ -2,12 +2,16 @@ import mysql.connector
 from mysql.connector import errorcode
 
 conexao = mysql.connector.connect(host='192.168.56.101', database='estudos', user='denirow', password='abc@123')
+conectou = conexao.is_connected()
 
-if conexao.is_connected():
+if conectou == True:
     banco='SGBD conectado com sucesso!'
     print(banco)
     cursor = conexao.cursor()
-    cursor.execute("select * from tb_user where user_name='alecia'")
+    cursor.execute("select * from tb_user where user_name='denys'")
+    resultado = cursor.fetchall()
+    print(resultado)
+else:
+    print('Banco de dados não conectado.')
 
-resultado = cursor.fetchall()
-print(resultado)
+
