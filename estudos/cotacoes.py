c6f4a026cafef9ea366852eda87fe3abfc61c025
Denys Silva <denirow@gmail.com>
2021-12-18 13:16:58 -0300

    v01

--- estudos/cotacoes.py
+++ estudos/cotacoes.py
@@ -8,3 +8,11 @@ dolar = cotacoes['USDBRL']['bid']
 print(dolar)
 euro = cotacoes['EURBRL']['bid']
 print(euro)
+
+testa_site = requests.get('http://www.uol.com.br')
+print(testa_site)
+
+if str(testa_site) == '<Response [200]>':
+    print('teste ok')
+else:
+    print('teste n ok')
