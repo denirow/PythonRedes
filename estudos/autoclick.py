3c32660bef4efb6e47cb3460c087d66edd6c12ed
Denys Silva <denirow@gmail.com>
2022-01-11 14:00:25 -0300

    v01

--- estudos/autoclick.py
+++ estudos/autoclick.py
@@ -1,5 +1,13 @@
 import pyautogui
+import time
+
+print(pyautogui.size())
+
+print(pyautogui.position())
 
 pyautogui.keyDown('alt')
-pyautogui.press(['tab'])
-pyautogui.keyUp('alt')
+pyautogui.press('tab')
+pyautogui.press('alt')
+pyautogui.write('automatizar pra ficar rico porra !!!')
+pyautogui.press('enter')
+pyautogui.write('o denirow se garante nos programas !!!')
