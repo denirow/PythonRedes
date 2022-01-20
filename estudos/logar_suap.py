5d18e97f26592a49de7f2a4a6268d87c0f938bf9
Denys Silva <denirow@gmail.com>
2022-01-20 20:12:32 -0300

    v01

--- estudos/logar_suap.py
+++ estudos/logar_suap.py
@@ -11,27 +11,27 @@ time.sleep(1)
 pyautogui.hotkey('ctrl','t')
 pyautogui.write('https://cav.receita.fazenda.gov.br/')
 pyautogui.press('enter')
-time.sleep(3)
+time.sleep(5)
 
 # Inserir CPF
-pyautogui.click(x=-782, y=440)
+pyautogui.click(x=956, y=440)
 pyautogui.write('04173636482')
 time.sleep(1)
 
 # Inserir Código de Acesso
-pyautogui.click(x=-782, y=501)
+pyautogui.click(x=956, y=501)
 pyautogui.write('168145753957')
 time.sleep(1)
 
 # Inserir Senha
-pyautogui.click(x=-782, y=568)
+pyautogui.click(x=956, y=568)
 pyautogui.write('D3nys0781')
 time.sleep(1)
 
 # Entrar no site
-pyautogui.click(x=-657, y=615)
+pyautogui.click(x=1096, y=613)
 time.sleep(2)
-pyautogui.click(x=-1511, y=385)
+pyautogui.click(x=122, y=394)
 time.sleep(4)
 
 # clicar nas pendências de 2021
