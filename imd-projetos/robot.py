81bb7d50188d945e9c64ae01d655b10f02f4677b
Denys Silva <denirow@gmail.com>
2022-05-05 09:58:40 -0300

    v0.0.1

--- imd-projetos/robot.py
+++ imd-projetos/robot.py
@@ -2,6 +2,7 @@ import os, shutil
 from time import sleep
 
 path="C:\denys"
+rm_win="del" +" "+ path +"\* /Q"
 
 if os.path.isdir (path):
 
@@ -9,6 +10,12 @@ if os.path.isdir (path):
         print("NÃO EXISTEM arquivos no diretório" +" "+ path +".")
     else:
         print("EXISTEM arquivos no diretório" +" "+ path +".")
+        print("--")
+        print("Excluindo arquivos.")
+        sleep(2)
+        os.system(rm_win)
+        print("Arquivos excluídos.")
+    
 else:
     print("O diretório" +" "+ path +" não existe.")
     print("--")
