20913a9c96301ba616f4a213bd95f98b14b184e0
Denys Silva <denirow@gmail.com>
2023-03-06 16:09:51 -0300

    Update Os.Popen

--- estudos/split.py
+++ estudos/split.py
@@ -1,7 +1,10 @@
 import os
 
-#get_kube = os.system("kubectl get pods -n feature | grep 1776 | grep backend")
-get_kube = "smartone-backend-1776-5bd989d599-j4p4s        4/4     Running     0          14h"
+ns = "feature"
+id = "1776"
+system = "backend"
+
+get_kube = os.popen("kubectl get pods -n " + ns + "| grep " + id + " | grep " + system).read()
 print('Aqui eh o comando completo')
 print(get_kube.split())
 print()
@@ -14,10 +17,10 @@ print(get_kube)
 print()
 
 print("aqui eh o filtro da primeira parte do comando splitado")
-print(get_kube[2])
+print(get_kube[1])
 print()
 
 if (get_kube[1]) == "backend":
-        print("aqui é o backend")
+    print("aqui é o backend")
 else:
     print("aqui não é o backend")
