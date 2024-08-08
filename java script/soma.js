fe7a4a2dc66bcebcf18dff2ef1f1ba865b1eaa1a
Denys Alexandre <denys.alexandre@smartsuite.imd.ufrn.br>
2024-08-08 14:56:07 -0300

    Update somar_html

---
+++ java script/soma.js
@@ -0,0 +1,14 @@
+// Definindo os dois números
+let numero1 = 5;
+let numero2 = 3;
+
+// Função para somar os dois números
+function somar(a, b) {
+    return a + b;
+}
+
+// Chamando a função e armazenando o resultado
+let resultado = somar(numero1, numero2);
+
+// Exibindo o resultado no console
+console.log("A soma de " + numero1 + " e " + numero2 + " é: " + resultado);
